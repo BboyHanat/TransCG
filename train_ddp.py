eb94857e57677e26d9ce0bd1ef09d0bcd0141628
hanat <1608273445@qq.com>
2023-08-03 20:05:26 +0800

    fix tensorboard stuck issue

--- train_ddp.py
+++ train_ddp.py
@@ -68,7 +68,8 @@ def dist_trainer(local_rank, dist_num: int, config: dict):
 
 
     if os.path.isfile(checkpoint_file):
-        checkpoint = torch.load(checkpoint_file, map_location="cpu")
+        gpu_device = torch.device('cuda:{}'.format(local_rank))
+        checkpoint = torch.load(checkpoint_file, map_location=gpu_device)
         network_model.load_state_dict(checkpoint['model_state_dict'])
         start_epoch = checkpoint['epoch']
         checkpoint_metrics = checkpoint['metrics']
