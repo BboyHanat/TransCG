3dde94cbe6714c83265c516b42d8058326f8d212
hanat <1608273445@qq.com>
2023-08-09 10:56:14 +0800

    add swin transformer finetuning

--- sample_inference.py
+++ sample_inference.py
@@ -5,18 +5,19 @@ from PIL import Image
 from inference import Inferencer
 
 
-def draw_point_cloud(color, depth, camera_intrinsics, use_mask = False, use_inpainting = True, scale = 1000.0, inpainting_radius = 5, fault_depth_limit = 0.2, epsilon = 0.01):
+def draw_point_cloud(color, depth, camera_intrinsics, use_mask=False, use_inpainting=True, scale=1000.0,
+                     inpainting_radius=5, fault_depth_limit=0.2, epsilon=0.01):
     """
     Given the depth image, return the point cloud in open3d format.
     The code is adapted from [graspnet.py] in the [graspnetAPI] repository.
     """
     d = depth.copy()
     c = color.copy() / 255.0
-    
+
     if use_inpainting:
         fault_mask = (d < fault_depth_limit * scale)
         d[fault_mask] = 0
-        inpainting_mask = (np.abs(d) < epsilon * scale).astype(np.uint8)  
+        inpainting_mask = (np.abs(d) < epsilon * scale).astype(np.uint8)
         d = cv2.inpaint(d, inpainting_mask, inpainting_radius, cv2.INPAINT_NS)
 
     fx, fy = camera_intrinsics[0, 0], camera_intrinsics[1, 1]
@@ -28,7 +29,7 @@ def draw_point_cloud(color, depth, camera_intrinsics, use_mask = False, use_inpa
     points_z = d / scale
     points_x = (xmap - cx) / fx * points_z
     points_y = (ymap - cy) / fy * points_z
-    points = np.stack([points_x, points_y, points_z], axis = -1)
+    points = np.stack([points_x, points_y, points_z], axis=-1)
 
     if use_mask:
         mask = (points_z > 0)
@@ -43,30 +44,29 @@ def draw_point_cloud(color, depth, camera_intrinsics, use_mask = False, use_inpa
     return cloud
 
 
-inferencer = Inferencer()
+# inferencer = Inferencer()
+inferencer = Inferencer(cfg_path="configs/inference.yaml")
 
-rgb = np.array(Image.open('/home/hanat/workspace/dataset/depth_completion/color/51.png'), dtype = np.float32)
-depth = np.array(Image.open('/home/hanat/workspace/dataset/depth_completion/depth/51.png'), dtype = np.float32)
-depth_gt = np.array(Image.open('/home/hanat/workspace/dataset/depth_completion/depth/51.png'), dtype = np.float32)
+rgb = np.array(Image.open('/home/hanat/workspace/dataset/depth_completion_cup/color/86.png'), dtype=np.float32)
+depth = np.array(Image.open('/home/hanat/workspace/dataset/depth_completion_cup/depth/86.png'), dtype=np.float32)
+depth_gt = np.array(Image.open('/home/hanat/workspace/dataset/depth_completion_cup/depth/86.png'), dtype=np.float32)
 
 depth = depth / 1000
 depth_gt = depth_gt / 1000
 
-res, depth = inferencer.inference(rgb, depth, target_size = (640, 480), depth_coefficient = 100, inpainting = True)
+res, depth = inferencer.inference(rgb, depth, target_size=(640, 480), depth_coefficient=3, inpainting=True)
 
 # cam_intrinsics = np.load('/home/hanat/workspace/dataset/camera_intrinsics/1-camIntrinsics-D435.npy')
 cam_intrinsics = np.asarray([[608.0112915, 0., 329.24252319],
-                  [0., 608.22619629, 248.08976746],
-                  [0., 0., 1.]], dtype=np.float32)
+                             [0., 608.22619629, 248.08976746],
+                             [0., 0., 1.]], dtype=np.float32)
 
 res = np.clip(res, 0.3, 1.0)
 depth = np.clip(depth, 0.3, 1.0)
 
-cloud = draw_point_cloud(rgb, res, cam_intrinsics, scale = 1.0)
-cloud_gt = draw_point_cloud(rgb, depth_gt, cam_intrinsics, scale = 1.0)
+cloud = draw_point_cloud(rgb, res, cam_intrinsics, scale=1.0)
+cloud_gt = draw_point_cloud(rgb, depth_gt, cam_intrinsics, scale=1.0)
 
 frame = o3d.geometry.TriangleMesh.create_coordinate_frame(0.1)
-sphere = o3d.geometry.TriangleMesh.create_sphere(0.002,20).translate([0,0,0.490])
+sphere = o3d.geometry.TriangleMesh.create_sphere(0.002, 20).translate([0, 0, 0.490])
 o3d.visualization.draw_geometries([cloud, frame, sphere])
-
-
